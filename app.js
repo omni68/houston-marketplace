146e4d1b92a834bb4a27264f8efde6d7c7bfba21
Josh Jones <joshua.jones@parivedasolutions.com>
2015-02-17 18:03:04 -0600

    initial setup; able to login and load app code;

---
+++ app.js
@@ -0,0 +1,10 @@
+function App() {
+	this.name = "Houston Marketplace"
+	init();
+}
+
+/* function declarations */
+function init() {
+	// initialize the app
+	alert('hello world!');
+}
---
+++ libraries/facebook.js
@@ -0,0 +1,46 @@
+window.fbAsyncInit = function() {
+    FB.init({
+        appId      : '351070228420063',
+        xfbml      : true,
+        version    : 'v2.2'
+    });
+
+    /* Function assignments */
+    FB._onGetLoginStatusResponse = onGetLoginStatusResponse;
+    FB._onNotAuthenticated = onNotAuthenticated;
+
+    // 
+    FB.getLoginStatus(FB._onGetLoginStatusResponse);
+};
+
+(function(d, s, id){
+   var js, fjs = d.getElementsByTagName(s)[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement(s); js.id = id;
+   js.src = "//connect.facebook.net/en_US/sdk.js";
+   fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+/* Function declarations */
+function onGetLoginStatusResponse(response) {
+    if(response.status === 'connected') {
+        console.log('Logged in.');
+        window.App = new App();
+    }
+    else {
+        console.log('Not logged in.');
+        FB.login(FB._onNotAuthenticated);
+    }
+};
+
+function onNotAuthenticated(response) {
+    if (response.authResponse) {
+         console.log('Welcome!  Fetching your information.... ');
+         FB.api('/me', function(response) {
+              console.log('Good to see you, ' + response.name + '.');
+         });
+         window.App = new App();
+    } else {
+        console.log('User cancelled login or did not fully authorize.');
+    }
+};
